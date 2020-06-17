4800b5548d529d1356a7901e3b4248048feabda5
李白云 <1103020596@qq.com>
2020-06-17 13:49:17 +0800

    简单搜书测试

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <h1>Home组件</h1>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <el-input placeholder="请输入书名" v-model="bookName">
+        <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+      </el-input>
+    </el-header>
+    <el-main>Main</el-main>
+  </el-container>
 </template>
 
 <script>
   export default {
-    name: 'Home'
+    name: 'Home',
+    data () {
+      return {
+        bookName: ''
+      }
+    },
+    created () {
+    },
+    methods: {
+      async search () {
+        const { data: res } = await this.$http.post('/book', { bookName: this.bookName })
+        console.log(res)
+      }
+    }
   }
 </script>
 
 <style lang="less" scoped>
+  .home-container {
+    height: 100%;
+  }
 
+  .el-header {
+    background-color: #409EFF;
+    display: flex;
+    align-items: center;
+  }
+
+  .el-main {
+    background-color: #E4E7ED;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Container, Message, Header, Main, Input } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Main)
+Vue.use(Input)
+Vue.prototype.$message = Message
